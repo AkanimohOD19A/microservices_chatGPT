3a395f97fb973e0f30de64d51e9aff88f72cb788
AfroLogicInsect <danielamahtoday@gmail.com>
2023-03-02 17:54:28 +0100

    completed v2

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-import os
-
 import streamlit as st
 import openai
 import pandas as pd
+from datetime import date
+from streamlit import caching
+# import hydralit_components as hc
 
-st.title("Chatting with ChatGPT")
-st.sidebar.header("Instructions")
-st.sidebar.info(
-    '''This is a web application that allows you to interact with 
-       the OpenAI API's implementation of the ChatGPT model.
-       Enter a **query** in the **text box** and **press enter** to receive 
-       a **response** from the ChatGPT
-       '''
-)
+# st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
 
-# Set the model engine and your OpenAI API key
-model_engine = "text-davinci-003"
-openai.api_key = st.secrets["Openai_SECRET_KEY"]
+## Handling Cache
+def cache_clear_dt(dummy):
+    clear_dt = date.today()
+    return clear_dt
 
+if cache_clear_dt("dummy")<date.today():
+    caching.clear_cache()
 
-# os.environ["Openai_SECRET_KEY"] ==  st.secrets["Openai_SECRET_KEY"]
-
+def create_action(adj_response_type, response_type):
+    # for adjective in adj_response_type:
+    #     prefix_adj = adjective.join(adjective).split(',')
+    strList = [str(i) for i in adj_response_type]
+    myString = ", ".join(strList)
+    prefix_keyword = myString + " and " + str(response_type)
+    st.sidebar.warning(prefix_keyword)
+    return prefix_keyword
 
 def ChatGPT(user_query):
     '''
@@ -40,46 +42,57 @@ def ChatGPT(user_query):
     response = completion.choices[0].text
     return response
 
-
-## Read a list of adjectives and actions
-adjs = pd.read_csv('./prefix_data/adjectives.csv')
-actions = pd.read_csv('./prefix_data/actions.csv')
-
-adj_response_type = st.sidebar.multiselect("Please specify keywords", adjs)
-response_type = st.sidebar.selectbox("Please specify action", actions)
-def create_action(adj_response_type, response_type):
-    # for adjective in adj_response_type:
-    #     prefix_adj = adjective.join(adjective).split(',')
-    strList = [str(i) for i in adj_response_type]
-    myString = ", ".join(strList)
-    prefix_keyword = myString + " and " + str(response_type)
-    st.sidebar.warning(prefix_keyword)
-    return prefix_keyword
-
-
-def main():
+@st.cache
+def api_call_on(query):
     '''
     This function gets the user input, pass it to ChatGPT function and
     displays the response
     '''
-    # Get user input
-    user_query = st.text_input("Enter query here, to exit enter :q", "what is Python?")
-    if user_query != ":q" or user_query != "":
-        # Pass the query to the ChatGPT function
-        if adj_response_type != "" and response_type != "":
-            prefix_keyword = create_action(adj_response_type, response_type)
-            prefix_query = f'A {prefix_keyword} response to {user_query}'
-            response = ChatGPT(prefix_query)
-            return st.write(f"{prefix_query} {response}")
-        else:
-            response = ChatGPT(user_query)
-            return st.write(f"{user_query} {response}")
+
+    response = ChatGPT(query)
+    return response
 
 
-if __name__ == '__main__':
-    main()
+# Set the model engine and your OpenAI API key
+model_engine = "text-davinci-003"
+openai.api_key = st.secrets["Openai_SECRET_KEY"]
 
+
+st.title("Run a Query with ChatGPT")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    '''This is a web application that allows you to interact with 
+       the OpenAI API's implementation of the ChatGPT model.
+       Enter a **query** in the **text box** and **press enter** to receive 
+       a **response** from the ChatGPT
+       '''
+)
+# Get user input
+user_query = st.text_input("Enter query here, to exit enter :q", "what is Python?")
+
+
+# os.environ["Openai_SECRET_KEY"] ==  st.secrets["Openai_SECRET_KEY"]
+option = st.sidebar.selectbox("How would you like to run your query?", ("Use Keywords", "Just run"))
+if option == "Use Keywords":
+    st.write('You selected: ', option)
+    ## Read a list of adjectives and actions
+    adjs = pd.read_csv('./prefix_data/adjectives.csv')
+    actions = pd.read_csv('./prefix_data/actions.csv')
+
+    adj_response_type = st.sidebar.multiselect("Please specify keywords", adjs)
+    response_type = st.sidebar.selectbox("Please specify action", actions)
+    if adj_response_type != "" and response_type != "":
+        prefix_keyword = create_action(adj_response_type, response_type)
+        prefix_query = f'A {prefix_keyword} response to {user_query}'
+        if st.sidebar.button("Run"):
+            with st.spinner('Wait for it...'):
+                response = api_call_on(prefix_query)
+                st.success(f"{response}")
+else:
+    response = api_call_on(user_query)
+    st.success(f"{response}")
 
 # ## To do
-# hide secret keys
-# add buttons
+# Collapse option to use keywords
+# Add cache
+# Add loader
